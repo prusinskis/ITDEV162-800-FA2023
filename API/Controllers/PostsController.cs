8029455a9f0ddca8375e6fc17b17ca044276a06b
Sherise Prusinski <98294721+prusinskis@users.noreply.github.com>
2023-10-15 11:50:33 -0500

    Create an endpoint to retrieve a single Post

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -19,11 +19,32 @@ namespace API.Controllers
             this.context = context;
         }
 
-        // GET api/posts
+        /// <summary>
+        /// GET api/posts 
+        /// </summary>
+        /// <returns> A list of posts</returns>
         [HttpGet(Name = "GetPosts")]
         public ActionResult<List<Post>> Get()
         {
             return this.context.Posts.ToList();
+        }
+        
+        /// <summary>
+        /// GET api/post/[id]
+        /// </summary>
+        /// <param name="id">Post id</param>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            var post = this.context.Posts.Find(id);
+
+            if(post is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
         }
     }
 }
