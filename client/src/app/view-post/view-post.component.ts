be8c8ae0cc95353a14719c8a4811320ae6aef3d7
Sherise Prusinski <98294721+prusinskis@users.noreply.github.com>
2023-11-05 23:07:10 -0600

    Build out ViewPostComponent

--- client/src/app/view-post/view-post.component.ts
+++ client/src/app/view-post/view-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-post',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-post.component.css']
 })
 export class ViewPostComponent implements OnInit {
+  post: any = {}
 
-  constructor() { }
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getPost();
   }
 
+  getPost() {
+    let id = this.route.snapshot.paramMap.get('id');
+    this.http.get('http://localhost:5128/api/posts/${id}').subscribe(post=> {
+      this.post = post;
+    }
+    );
+  }
 }
