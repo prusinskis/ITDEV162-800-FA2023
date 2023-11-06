9ff0da61b995794282b5a9b5c0e806572d6d4f8f
Sherise Prusinski <98294721+prusinskis@users.noreply.github.com>
2023-11-05 19:06:01 -0600

    Move post list

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  posts:any;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+      this.http.get('http://localhost:5128/api/posts').subscribe(
+      response => {this.posts = response; },
+      error => {console.log(error) }
+    );
   }
 
 }
